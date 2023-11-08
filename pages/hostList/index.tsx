395fa015ee7081282bd51b5f0cd18a3b5f24da52
developer-jyyun <developer.jyyun@gmail.com>
2023-11-09 05:11:31 +0900

    Fix: Resolve issue with tab

--- pages/hostList/index.tsx
+++ pages/hostList/index.tsx
@@ -7,98 +7,56 @@ import HostListItem from './components/HostListItem';
 import styles from './hostList.module.scss';
 
 export default function HostList() {
-  const [selectedTab, setSelectedTab] = useState<number | string>(0);
   const [hosts, setHosts] = useState<Host[]>([]);
+  const [selectedTab, setSelectedTab] = useState<number>(0);
 
-  useEffect(() => {
-    // 호스트 정보를 불러오는 비동기 함수
-    const fetchHosts = async () => {
-      try {
-        // Firestore에서 호스트 정보를 가져옴
-        const location = String(selectedTab);
-        const response = await fetchHostsFromFirestore(location);
-        setHosts(response);
-      } catch (error) {
-        console.error('호스트 정보 불러오기 오류:', error);
-      }
-    };
+  const locations = ['busan', 'jeju', 'gyeongju', 'jeonju'];
 
-    fetchHosts();
-  }, [selectedTab]);
-
-  const fetchHostsByLocation = async location => {
+  const fetchHosts = async (location: string) => {
     try {
       const response = await fetchHostsFromFirestore(location);
       setHosts(response);
     } catch (error) {
-      console.log('호스트 정보 불러오기 오류:', error);
+      console.error('호스트 정보 불러오기 오류:', error);
     }
   };
+
+  useEffect(() => {
+    // 초기 로드: 부산
+    fetchHosts(locations[selectedTab]);
+  }, []);
 
   return (
     <section className={styles.container}>
       <button type="submit" onClick={addHostsToFirestore}>
         호스트 저장
       </button>
-      <article>
-        {/* <h2 className={hostListStyles.title}>숙소 목록</h2> */}
-
-        <Tabs
-          selectedIndex={selectedTab}
-          onSelect={index => {
-            setSelectedTab(index);
-            const locations = ['busan', 'jeju', 'gyeongju', 'jeonju'];
-            fetchHostsByLocation(locations[index]);
-          }}
-        >
-          <TabList>
-            <Tab onClick={() => setSelectedTab(0)}>부산</Tab>
-            <Tab onClick={() => setSelectedTab(1)}>제주</Tab>
-            <Tab onClick={() => setSelectedTab(2)}>경주</Tab>
-            <Tab onClick={() => setSelectedTab(3)}>전주</Tab>
-          </TabList>
-          <TabPanel>
-            <h3 className={styles.location}> # 부산</h3>
+      <Tabs
+        selectedIndex={selectedTab}
+        onSelect={index => {
+          const selectedLocation = locations[index];
+          setSelectedTab(index);
+          fetchHosts(selectedLocation);
+        }}
+      >
+        <TabList>
+          {locations.map(location => (
+            <Tab key={location}>{location}</Tab>
+          ))}
+        </TabList>
+        {locations.map(location => (
+          <TabPanel key={location}>
+            <h3 className={styles.location}>#{location}</h3>
             <ul className={styles.itemList}>
               {hosts
-                .filter(host => host.location === 'busan')
+                .filter(host => host.location === location)
                 .map(host => (
                   <HostListItem key={host.id} host={host} />
                 ))}
             </ul>
           </TabPanel>
-          <TabPanel>
-            <h3 className={styles.location}> # 제주</h3>
-            <ul className={styles.itemList}>
-              {hosts
-                .filter(host => host.location === 'jeju')
-                .map(host => (
-                  <HostListItem key={host.id} host={host} />
-                ))}
-            </ul>
-          </TabPanel>
-          <TabPanel>
-            <h3 className={styles.location}> # 경주</h3>
-            <ul className={styles.itemList}>
-              {hosts
-                .filter(host => host.location === 'gyeongju')
-                .map(host => (
-                  <HostListItem key={host.id} host={host} />
-                ))}
-            </ul>
-          </TabPanel>
-          <TabPanel>
-            <h3 className={styles.location}> # 전주</h3>
-            <ul className={styles.itemList}>
-              {hosts
-                .filter(host => host.location === 'jeonju')
-                .map(host => (
-                  <HostListItem key={host.id} host={host} />
-                ))}
-            </ul>
-          </TabPanel>
-        </Tabs>
-      </article>
+        ))}
+      </Tabs>
     </section>
   );
 }
